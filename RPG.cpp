8ca951c1c6537cf29efee77e54c23fd3021a4704
csiziszebasztian <tanulas88@gmail.com>
2020-09-19 09:24:55 +0200

    Add Program and How to use description to README.md

---
+++ RPG.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc == 1) {
+        std::cerr << "Nem adott meg parametert.!" << std::endl;
+    }
+
+    else if (argc < 7) {
+        std::cerr << "Nem adott meg eleg adatot!" << std::endl;
+    }
+
+    else {
+
+    
+        
+    }
+}
