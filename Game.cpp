c03cdafca6c71bc452138e580775b0dd48e870d5
csiziszebasztian <tanulas88@gmail.com>
2020-09-19 17:40:13 +0200

    Add constructor and destructor to Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,31 @@
+#include "Game.h"
+#include <sstream>
+#include <iostream>
+
+Game::Game(char** argv)
+{
+    std::stringstream ss;
+    std::string p1, p2;
+    short hp1, hp2, dmg1, dmg2;
+
+    for (argv++; *argv != NULL; ++argv) {
+        ss << *argv << '\n';
+    }
+
+    ss >> p1 >> hp1 >> dmg1;
+    ss >> p2 >> hp2 >> dmg2;
+
+    players.push_back(new Player(p1, hp1, dmg1));
+    players.push_back(new Player(p2, hp2, dmg2));
+
+}
+
+Game::~Game()
+{
+
+    for (auto player : players)
+    {
+        delete player;
+    }
+ 
+}
