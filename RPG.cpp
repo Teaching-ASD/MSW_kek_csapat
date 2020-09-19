31c6d9540af0279873275c044b8d93b44e90c34d
csiziszebasztian <tanulas88@gmail.com>
2020-09-19 17:11:42 +0200

    Add backend Game object initialization run and detelete to RPG.cpp

--- RPG.cpp
+++ RPG.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "Game.h"
 
 int main(int argc, char** argv)
 {
     if (argc == 1) {
-        std::cerr << "Nem adott meg parametert.!" << std::endl;
+        std::cerr << "Nem adott meg parametert!" << std::endl;
     }
 
     else if (argc < 7) {
@@ -12,7 +13,9 @@ int main(int argc, char** argv)
 
     else {
 
-    
+        Game* game = new Game(argv);
+        game->Fight();
+        delete game;
         
     }
 }
