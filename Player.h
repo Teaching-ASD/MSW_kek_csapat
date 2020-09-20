335d910ed4bb80b4a4f4c87f4fd06d580b29f48f
jonasbence <jbence123@gmail.com>
2020-09-20 17:01:19 +0200

    Creat Player.h and add source to Player.h

---
+++ Player.h
@@ -0,0 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+#include <string>
+
+class Player
+{
+	const std::string name;
+	short hp;
+	const unsigned short dmg;
+
+public:
+	Player(std::string name, short hp, unsigned short dmg);
+    std::string getName() const;
+    short getHP() const;
+    void setHP(short newHP);
+    unsigned short getDMG() const;
+    void DMG(Player* enemy);
+    std::string toString();
+
+
+};
+
+#endif
