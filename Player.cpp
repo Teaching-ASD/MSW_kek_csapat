8a7f40c0e16d076d7038be09a0ac789c41f61aa7
jonasbence <jbence123@gmail.com>
2020-09-20 17:11:05 +0200

    Add setter and getter Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -5,3 +5,20 @@ Player::Player(std::string name, short hp, unsigned short dmg): name(name), hp(h
 
 }
 
+std::string Player::getName() const {
+    return name;
+}  
+
+short Player::getHP() const {
+  return hp; 
+}
+
+void Player::setHP(short newHP){
+    hp=newHP;
+}
+    
+
+unsigned short Player::getDMG() const{
+    return dmg;
+}
+  
