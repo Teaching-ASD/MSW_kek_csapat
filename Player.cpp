becd579361e65c6ed110002a73ff7ddc06fb05ad
csiziszebasztian <tanulas88@gmail.com>
2020-09-26 11:16:41 +0200

    Add parseUnit definition and redefine constructor to Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,8 @@
 #include "Player.h"
+#include <fstream>
+#include <sstream>
 
-
-Player::Player(std::string name, short hp, unsigned short dmg): name(name), hp(hp), dmg(dmg) {
+Player::Player(std::string name, int hp, int dmg): name(name), hp(hp), dmg(dmg) {
 
 }
 
@@ -24,10 +25,54 @@ void Player::DMG(Player* enemy) {
     }
 }   
 
-
-
 std::string Player::toString() 
 {
 	return name + ": HP:" + std::to_string(hp) + ", DMG: " + std::to_string(dmg);
 }
 
+
+Player* Player::parseUnit(std::string fileName){
+
+
+    std::ifstream inf;
+    inf.exceptions(std::ifstream::failbit);
+    inf.open(fileName);
+    std::stringstream ss;
+
+    char start_end;
+    char sep;
+
+    std::string type;
+
+    std::string name, sHp, sDmg;
+    int hp, dmg;
+
+    if (inf.is_open()) {
+      
+      ss << inf.rdbuf();
+      inf.close();
+
+      ss >> start_end;
+      //Read String
+      ss >> type >> sep >> name;
+      name.erase(name.begin(), name.begin()+1);
+      name.erase(name.end()-2, name.end());
+   
+
+      //Read Integer
+      ss >> type >> sep >> sHp;
+      sHp.erase(sHp.end()-1);
+      ss >> type >> sep >> sDmg;
+
+      hp = std::stoi(sHp);
+      dmg = std::stoi(sDmg);
+
+      Player* p = new Player(name,hp,dmg);
+      return p; 
+
+    }
+    else {
+        return nullptr;
+    }
+
+}
