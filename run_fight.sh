5fda61c61da3744046d3f5122801c9e53e208fe4
smauzerrichard <smauzer.richard47@gmail.com>
2020-09-26 20:33:53 +0200

    add run_fight

---
+++ run_fight.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+rm winner.txt
+
+for i in player1 player2 player3
+do
+    for j in player1 player2 player3
+    do
+    if [ $i != $j ]
+    then
+         ./a.out Units/$i.json Units/$j.json >> winner.txt
+    fi
+    done
+done
