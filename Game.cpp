2952e646aae4d63723057d4c835a090e540c028d
smauzerrichard <smauzer.richard47@gmail.com>
2020-10-11 15:28:53 +0200

    start programming logical operation

--- Game.cpp
+++ Game.cpp
@@ -24,21 +24,37 @@ void Game::Fight()
     bool round=true;
 
     while(!isEnd()){
-
+        //*Frist Round*//
         switch (round)
         {
         case true:
+                std::cout << players[0]->getName() << "->" << players[1]->getName() << std::endl;
                players[1]->DMG(players[0]);
                round=false;
             break;
         
         case false:
+                std::cout << players[1]->getName() << "->" << players[0]->getName() << std::endl;
                players[0]->DMG(players[1]);
                round=true;
             break;
         }
-    }
+    //*Secound Round
+    //Ha a Player 1 atksp nagyobb mint a player 2 
+        if(players[0]->atksp > player[1]->atksp){
+            //kivonom a player 0 nak az atakspeedjeből a player 1 atakspedjet
+            //majd a kivont értéket megkapja a player 0 és uj értéket kap a player1
+            std::cout << players[0]->getName() << "->" << players[1]->getName() << std::endl;
+            players[1]->DMG(players[0]);
+        }return
+        else{
+            std::cout << players[1]->getName() << "->" << players[0]->getName() << std::endl;
+            players[0]->DMG(players[1]);
 
+        }return
+
+    }
+    std::cout << toString() << std::endl;
     std::cout << getResult() << std::endl;
 
 }
