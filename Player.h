38e02ce86e646be6d734a3193f27dd43ebd73b1b
smauzerrichard <smauzer.richard47@gmail.com>
2020-10-11 15:30:04 +0200

    declarated atksp

--- Player.h
+++ Player.h
@@ -9,15 +9,16 @@ class Player
 	
     const std::string name;
     int hp, dmg;
+    float atksp;
 
 public:
-	Player(std::string name, int hp, int dmg);
+	Player(std::string name, int hp, int dmg, float atksp);
     
     //Getters
     std::string getName() const;
     short getHP() const;
     unsigned short getDMG() const;
-    
+    float getAtksp() const;
     //Functions
     void DMG(Player* enemy);
     std::string toString();
