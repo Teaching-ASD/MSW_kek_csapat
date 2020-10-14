1ef5c97a6013610dfb021264e3e879f4ba665844
smauzerrichard <58022443+smauzerrichard@users.noreply.github.com>
2020-10-14 19:59:49 +0200

    Revert "add atksp getter"
    This reverts commit 9a4494835f48a829701a04e848299af0b6146719.

--- Player.cpp
+++ Player.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <sstream>
 
-Player::Player(std::string name, int hp, int dmg, float atksp): name(name), hp(hp), dmg(dmg), atksp(atksp) {
+Player::Player(std::string name, int hp, int dmg): name(name), hp(hp), dmg(dmg) {
 
 }
 
@@ -17,10 +17,6 @@ short Player::getHP() const {
 unsigned short Player::getDMG() const{
     return dmg;
 }
-//add attackspeed getters
-float Player::getAtksp() const{
-    return atksp;
-}
 
 void Player::DMG(Player* enemy) {
     hp-=enemy->getDMG(); 
@@ -28,10 +24,10 @@ void Player::DMG(Player* enemy) {
         hp=0;
     }
 }   
-//add attackspeed
+
 std::string Player::toString() 
 {
-	return name + ": HP:" + std::to_string(hp) + ", DMG: " + std::to_string(dmg) + ", Atksp: " + std::to_string(atksp);
+	return name + ": HP:" + std::to_string(hp) + ", DMG: " + std::to_string(dmg);
 }
 
 
@@ -48,9 +44,8 @@ Player* Player::parseUnit(std::string fileName){
 
     std::string type;
 
-    std::string name, sHp, sDmg, sAtksp;
+    std::string name, sHp, sDmg;
     int hp, dmg;
-    float atksp;
 
     if (inf.is_open()) {
       
@@ -68,15 +63,11 @@ Player* Player::parseUnit(std::string fileName){
       ss >> type >> sep >> sHp;
       sHp.erase(sHp.end()-1);
       ss >> type >> sep >> sDmg;
-      //Floating point number
-      ss >> type >> sep >> sAtksp;
-      sAtksp.erase(sAtksp.end()-1);
 
       hp = std::stoi(sHp);
       dmg = std::stoi(sDmg);
-      atksp = std::stof(sAtksp)
 
-      Player* p = new Player(name,hp,dmg,atksp);
+      Player* p = new Player(name,hp,dmg);
       return p; 
 
     }
