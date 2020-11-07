a99a3a78af8032b058719ba334460fd447d75166
csiziszebasztian <tanulas88@gmail.com>
2020-11-07 10:43:55 +0100

    Refactor Monster class

---
+++ Monster.h
@@ -0,0 +1,41 @@
+/**
+ * \class Character
+ * 
+ * \ingroup Example package
+ *  
+ * \brief Character class
+ * 
+ * This is a Charater class. Character have name, health, damage, and cooldown.
+ * Character is base class for RPG fight.
+ * They can fight but they can't take experience.
+ * More complex game features in the Player child class.
+ * 
+ * \author smauzerrichard (smauzer.richard47@gmail.com)
+ * \version 1.0
+ * \date 2020-10-13
+ * 
+ */
+
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include <string>
+#include "Character.h"
+
+class Monster : public Character {
+   
+
+public:
+
+	Monster(
+    std::string name,
+    int hp, 
+    int dmg, 
+    double atksp) : Character(name, hp, dmg, atksp){}
+
+    static Monster parse(std::string json);
+
+  
+};
+
+#endif
