d2a64ab2a17b3a5d61df480cf76cc5a51df94cde
csiziszebasztian <tanulas88@gmail.com>
2020-12-04 10:28:25 +0100

    Add position defense damage to Monster

--- Monster.cpp
+++ Monster.cpp
@@ -3,32 +3,42 @@
 #include <vector>
 #include <stdexcept>
 
-Monster Monster::parse(const std::string& json){
+
+
+Monster* Monster::parse(const std::string& json){
 
    JSON jdm = JSON::parseFromFile(json); 
 
    std::vector<std::string> PlayerData {
     "name",
     "health_points",
-    "damage",
     "attack_cooldown",
+    "defense"
    };
 
 
     std::vector<std::string>::size_type i;
    for (i = 0; i < PlayerData.size(); i++)
    {
+
         if (!jdm.count(PlayerData.at(i)) ) {
             throw JSON::ParseException();
         }
     }
 
+    
+    Damage d;
+
+    if(jdm.count("damage")) d.physical=jdm.get<int>("damage");
+    if(jdm.count("magical-damage")) d.magical=jdm.get<int>("magical-damage");
+
     std::string name = jdm.get<std::string>("name");
     int hp = jdm.get<int>("health_points");
     int dmg = jdm.get<int>("damage");
     double atksp = jdm.get<double>("attack_cooldown");
+    int def = jdm.get<int>("defense");
     
-    Monster monster(name, hp, dmg, atksp);
+    Monster* monster = new Monster(name, hp, d.physical, d.magical, atksp, def);
 
     return monster;
 }
