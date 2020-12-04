3a156bd8c4f183e2c9f184e3a359d984a17c428b
csiziszebasztian <tanulas88@gmail.com>
2020-12-04 10:27:45 +0100

    Add map to project

---
+++ Map.h
@@ -0,0 +1,52 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+  
+  
+
+class Map {
+
+public:
+	typedef enum type 
+	{ 
+		Wall='#',
+		Free=' ',
+		Monsters='M',
+		Hero='H',
+		Monster='m',
+		Unknown
+	}type;
+
+private:
+	std::vector<std::string> storedGameMap;
+	std::size_t width=0;
+
+public:
+	Map();
+    Map(const std::string& filename);
+	Map::type get(int x, int y)const;
+	std::size_t getWidth() const;
+	std::size_t getHeight() const;
+	std::size_t getRowLenght(int r)const;
+	void setBlock(Map::type t, int x, int y);
+
+	class WrongIndexException : virtual public std::runtime_error {
+		public:
+		    explicit WrongIndexException(const std::string& msg) : std::runtime_error("Wrong index: " + msg) {}
+	};
+
+	class FileException : virtual public std::runtime_error {
+		public:
+		    explicit FileException(const std::string& msg) : std::runtime_error("File exception: " + msg) {}
+	};
+
+	friend bool operator==(const Map& map1, const Map& map2);
+
+};
+
+
+
+#endif //MAP_H
