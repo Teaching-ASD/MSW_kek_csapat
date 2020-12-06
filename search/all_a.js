63a62cc4af4af3b9a28c0a9314de6dbaa7c43bed
csiziszebasztian <57803004+csiziszebasztian@users.noreply.github.com>
2020-12-06 22:56:35 +0000

    Update gh-pages to output generated at 09ae0c1

--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['observersvgrenderer',['ObserverSVGRenderer',['../classObserverSVGRenderer.html',1,'']]],
   ['observertextrenderer',['ObserverTextRenderer',['../classObserverTextRenderer.html',1,'']]],
   ['occupiedexception',['OccupiedException',['../classGame_1_1OccupiedException.html',1,'Game']]],
   ['operator_3d_3d',['operator==',['../classHero.html#af0fda00004b588e86d04f6d842d1028a',1,'Hero']]]
--- search/classes_9.js
+++ search/classes_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['observersvgrenderer',['ObserverSVGRenderer',['../classObserverSVGRenderer.html',1,'']]],
   ['observertextrenderer',['ObserverTextRenderer',['../classObserverTextRenderer.html',1,'']]],
   ['occupiedexception',['OccupiedException',['../classGame_1_1OccupiedException.html',1,'Game']]]
 ];
