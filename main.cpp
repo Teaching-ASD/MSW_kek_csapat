c4cf5ba3e742866fff3a8892d71318b6dfea1f5e
csiziszebasztian <tanulas88@gmail.com>
2020-12-06 13:16:51 +0100

    Refact for MarkedMap and PrepareGame

--- main.cpp
+++ main.cpp
@@ -8,11 +8,7 @@
 #include <vector>
 #include <list>
 
-#include "JSON.h"
-#include "Hero.h"
-#include "Monster.h"
-#include "Map.h"
-#include "Game.h"
+#include "PreparedGame.h"
 
 
 
@@ -36,88 +32,12 @@ int main(int argc, char** argv){
     if (argc != 2) bad_exit(1);
     if (!std::filesystem::exists(argv[1])) bad_exit(2);
 
-    std::string hero_file;
-    std::list<std::string> monster_files;
-    try {
-        JSON scenario = JSON::parseFromFile(argv[1]); 
-        if (!(scenario.count("hero")&&scenario.count("monsters"))) bad_exit(3);
-        else {
-            hero_file=scenario.get<std::string>("hero");
-            JSON::list monster_file_list=scenario.get<JSON::list>("monsters");
-            for(auto monster_file : monster_file_list)
-                monster_files.push_back(std::get<std::string>(monster_file));
-        }
-    } catch (const JSON::ParseException& e) {bad_exit(4);}
-
-    Hero* hero=nullptr;
-    std::vector<Monster*> monsters;
-    Map* map=nullptr;
-
     try { 
-        hero = Hero::parse(hero_file);
-        for (const auto& monster_file : monster_files)
-            monsters.push_back(Monster::parse(monster_file));        
-        map = new Map("./Maps/Map1.txt");
-
-        Game game;
-
-        game.setMap(map);
-        game.putHero(hero, 1, 1);
-
-        game.putMonster(monsters[0],2,1);
-        game.putMonster(monsters[1],3,3);
-        game.putMonster(monsters[2],5,3);
-        game.putMonster(monsters[3],5,3);
-        game.putMonster(monsters[4],5,4);
-        game.putMonster(monsters[5],5,6);
-        game.putMonster(monsters[6],5,6);
-        game.putMonster(monsters[7],5,6);
-
-        game.run();
-
+        PreparedGame pg(argv[1]);
+        pg.run();
     } 
-    catch (const JSON::ParseException& e) {bad_exit(4);}
-    
-    catch(const Map::WrongIndexException& me){
-        std::cout << me.what() << std::endl;
-    }
-
-    catch(const Map::FileException& me){
-        std::cout << me.what() << std::endl;
-    }
-
-    catch(const Game::AlreadyHasHeroException& ge){
-        std::cout << ge.what() << std::endl;
-    }
-
-    catch(const Game::AlreadyHasUnitsException& ge){
-        std::cout << ge.what() << std::endl;
-    }
-    
-    catch(const Game::AlreadyStartedException& ge){
-        std::cout << ge.what() << std::endl;
-    }
-      
-    catch(const Game::MonsterAlreadyContains& ge){
-        std::cout << ge.what() << std::endl;
-    }
-    
-    catch(const Game::NotInitializedException& ge){
-        std::cout << ge.what() << std::endl;
-    }
-
-    catch(const Game::OccupiedException& ge){
-        std::cout << ge.what() << std::endl;
-    }
-
-    
-    delete hero;
-    delete map;
-
-    if(!monsters.empty()){
-        for(auto monster : monsters){
-            delete monster;
-        }
+    catch (const std::exception& ex ){
+        std::cout << ex.what() << std::endl;
     }
 
     return 0;
