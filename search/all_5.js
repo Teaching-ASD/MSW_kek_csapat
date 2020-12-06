ca6debbf6d20779771c18f0d85fc12abf1f79eba
csiziszebasztian <57803004+csiziszebasztian@users.noreply.github.com>
2020-12-06 18:57:06 +0000

    Update gh-pages to output generated at b2ffbcb

--- search/all_5.js
+++ search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['hero',['Hero',['../classHero.html',1,'Hero'],['../classHero.html#a50a7c8f50720f7b11248fc261673c53f',1,'Hero::Hero()']]],
+  ['hero',['Hero',['../classHero.html',1,'Hero'],['../classHero.html#a16f23959069f603375937b661e9eb8ae',1,'Hero::Hero()']]],
   ['herotextrenderer',['HeroTextRenderer',['../classHeroTextRenderer.html',1,'']]],
   ['hp',['hp',['../classCharacter.html#a273353643988a87a0ce03cff3870b2d9',1,'Character']]]
 ];
